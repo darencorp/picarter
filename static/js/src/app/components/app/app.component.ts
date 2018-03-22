15283042c44687041aab430a512c2d471952d51d
darencorp <nazar.mandebura@gmail.com>
2018-03-22 17:46:38 +0100

    main page

--- static/js/src/app/components/app/app.component.ts
+++ static/js/src/app/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true); //third parameter
+  }
+
+  scroll = (): void => {
+    //handle your scroll here
+    //notice the 'odd' function assignment to a class field
+    //this is used to be able to remove the event listener
+
+    console.log("scrolling")
+  };
 }

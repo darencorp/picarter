6825face19356f29eebd85555c6ce4e00b7338ce
darencorp <nazar.mandebura@gmail.com>
2018-03-24 12:25:05 +0100

    login form uikit dependency

--- static/js/src/app/components/app/app.component.ts
+++ static/js/src/app/components/app/app.component.ts
@@ -1,22 +1,44 @@
 import {Component} from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+declare var UIkit : any
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
 
-  ngOnInit() {
-    window.addEventListener('scroll', this.scroll, true); //third parameter
+export class AppComponent {
+
+  q = ''
+
+  constructor(private sanitizer: DomSanitizer) {
   }
 
-  scroll = (): void => {
-    //handle your scroll here
-    //notice the 'odd' function assignment to a class field
-    //this is used to be able to remove the event listener
 
-    console.log("scrolling")
-  };
+  title = 'app';
+
+  photos = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg',
+    '8.jpg',
+    '9.jpg',
+  ];
+
+  updateUrl(url) {
+    return this.sanitizer.bypassSecurityTrustStyle(`url(/static/img/${url})`);
+  }
+
+  openLoginForm() {
+    UIkit.modal("#login-form-l").show()
+  }
+
+  signIn() {
+    console.log("asdfghjkl")
+  }
 }
